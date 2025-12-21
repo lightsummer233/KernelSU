19cc8ad52c3e7f4849d1b56309bf6597f13381db
淡いなつ <93428659+lightsummer233@users.noreply.github.com>
2025-12-21 19:33:55 +0800

    kernel: Set manager signature size and hash to my private sign key

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -90,7 +90,7 @@ static bool check_block(struct file *fp, loff_t *pos, loff_t block_end, unsigned
 	if (certificate_size > INT_MAX || certificate_size > (u64)(certificates_end - *pos))
 		return false;
 
-#define CERT_MAX_LENGTH 1024
+#define CERT_MAX_LENGTH 2373
 	if (certificate_size != expected_size)
 		return false;
 
